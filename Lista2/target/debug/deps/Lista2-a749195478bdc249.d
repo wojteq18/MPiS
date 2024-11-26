5c13a2ed2a8949d91e50d54b2f6113a8221f7c08
Wojtaz <279730@student.pwr.edu.pl>
2024-11-26 17:35:23 +0100

    Poprawienie pdf'a

---
+++ Lista2/target/debug/deps/Lista2-a749195478bdc249.d
@@ -0,0 +1,5 @@
+/home/vostok/codes/MPiS/Lista2/target/debug/deps/libLista2-a749195478bdc249.rmeta: src/main.rs
+
+/home/vostok/codes/MPiS/Lista2/target/debug/deps/Lista2-a749195478bdc249.d: src/main.rs
+
+src/main.rs:
