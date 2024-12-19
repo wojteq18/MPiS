e476d08f0b07eff547325f50f8ae09474a52631c
Wojtaz <279730@student.pwr.edu.pl>
2024-12-19 13:12:08 +0100

    poprawki

--- Lista3/src/main.rs
+++ Lista3/src/main.rs
@@ -6,7 +6,7 @@ use std::io::{Write, BufWriter};
 pub fn calculate(n_values: Vec<usize>, repeats: usize) {
     let mut an_file = BufWriter::new(File::create("an_results.txt").unwrap());
     let mut bn_file = BufWriter::new(File::create("bn_results.txt").unwrap());
-    
+
     for &n in n_values.iter() {
         let mut rng = Mt64::default();
 
@@ -14,24 +14,23 @@ pub fn calculate(n_values: Vec<usize>, repeats: usize) {
             let mut urns = vec![0; n];
             let mut max_balls_an = 0;
             let mut max_balls_bn = 0;
-            //let mut an = 0;
 
             for i in 1..n {
-                //obliczmy an - maxymalna ilosc kul w urnie
+                // Obliczamy an - maksymalna liczba kul w urnie
                 let urn = rng.gen_range(0..n);
                 urns[urn] += 1;
                 if urns[urn] > max_balls_an {
                     max_balls_an = urns[urn];
                 }
 
-                //obliczmy bn - odejmujemy kule dodana, losujemy nastepna urne i wybieramy do ktorej z tych dwoch ma trafic kula
+                // Obliczamy bn - odejmujemy kulę dodaną, losujemy następną urnę i wybieramy, do której z tych dwóch ma trafić kula
                 urns[urn] -= 1;
-                
+
                 let mut urn2;
                 loop {
                     urn2 = rng.gen_range(0..n);
                     if urn != urn2 {
-                        break; // nie chcemy wylosowac urny, ktora juz wylosowalismy
+                        break; // Nie chcemy wylosować urny, którą już wylosowaliśmy
                     }
                 }
 
@@ -40,13 +39,16 @@ pub fn calculate(n_values: Vec<usize>, repeats: usize) {
                 } else {
                     urns[urn] += 1;
                 }
-                if max_balls_bn < urns[urn] {
-                    max_balls_bn = urns[urn];
-                }
-                else if max_balls_bn < urns[urn2] {
+
+                if urns[urn] > urns[urn2] {
+                    if urns[urn] > max_balls_bn {
+                        max_balls_bn = urns[urn];
+                    }
+                } else if urns[urn2] > max_balls_bn {
                     max_balls_bn = urns[urn2];
                 }
             }
+
             writeln!(an_file, "{} {}", n, max_balls_an).unwrap();
             writeln!(bn_file, "{} {}", n, max_balls_bn).unwrap();
         }
@@ -54,8 +56,8 @@ pub fn calculate(n_values: Vec<usize>, repeats: usize) {
 }
 
 fn main() {
-    let n_values: Vec<usize> = (1000..=100000).step_by(1000).collect(); //zakres wartosci n
-    let repeats = 5; //ilosc powtorzen dla kazdej wartosci n
+    let n_values: Vec<usize> = (10000..=1000000).step_by(10000).collect(); // Zakres wartości n
+    let repeats = 5; // Ilość powtórzeń dla każdej wartości n
 
     calculate(n_values, repeats);
 }
